00a4c12117d3fab10c38ff6be896562b0f93e073
chiru199231 <srai95273@gmail.com>
2026-07-05 11:26:12 +0530

    Refactor player data structure for Brazil and Norway

--- player.js
+++ player.js
@@ -1,323 +1,141 @@
 const tickets = [
+
 {
 ticket:1,
-brazil:{
-name:"Bremer",
-number:14,
-photo:"assets/brazil/bremer.jpg"
-},
-norway:{
-name:"Kristoffer Ajer",
-number:3,
-photo:"assets/norway/ajer.jpg"
-}
+brazil:{name:"Igor Thiago",number:25},
+norway:{name:"Henrik Falchener",number:23}
 },
 
 {
 ticket:2,
-brazil:{
-name:"Fabinho",
-number:17,
-photo:"assets/brazil/fabinho.jpg"
-},
-norway:{
-name:"Patrick Berg",
-number:6,
-photo:"assets/norway/patrickberg.jpg"
-}
+brazil:{name:"Roger Ibañez",number:24},
+norway:{name:"Sondre Langås",number:24}
 },
 
 {
 ticket:3,
-brazil:{
-name:"Igor Thiago",
-number:25,
-photo:"assets/brazil/igor.jpg"
-},
-norway:{
-name:"Jørgen Strand Larsen",
-number:11,
-photo:"assets/norway/larsen.jpg"
-}
+brazil:{name:"Danilo",number:13},
+norway:{name:"David Møller Wolfe",number:5}
 },
 
 {
 ticket:4,
-brazil:{
-name:"Alex Sandro",
-number:6,
-photo:"assets/brazil/alexsandro.jpg"
-},
-norway:{
-name:"Fredrik Aursnes",
-number:14,
-photo:"assets/norway/aursnes.jpg"
-}
+brazil:{name:"Douglas Santos",number:16},
+norway:{name:"Marcus Holmgren Pedersen",number:16}
 },
 
 {
 ticket:5,
-brazil:{
-name:"Lucas Paquetá",
-number:20,
-photo:"assets/brazil/paqueta.jpg"
-},
-norway:{
-name:"Morten Thorsby",
-number:2,
-photo:"assets/norway/thorsby.jpg"
-}
+brazil:{name:"Fabinho",number:17},
+norway:{name:"Fredrik André Bjørkan",number:15}
 },
 
 {
 ticket:6,
-brazil:{
-name:"Danilo",
-number:13,
-photo:"assets/brazil/danilo.jpg"
-},
-norway:{
-name:"Marcus Pedersen",
-number:16,
-photo:"assets/norway/pedersen.jpg"
-}
+brazil:{name:"Alex Sandro",number:6},
+norway:{name:"Kristoffer Ajer",number:3}
 },
 
 {
 ticket:7,
-brazil:{
-name:"Roger Ibañez",
-number:24,
-photo:"assets/brazil/ibanez.jpg"
-},
-norway:{
-name:"David Møller Wolfe",
-number:5,
-photo:"assets/norway/wolfe.jpg"
-}
+brazil:{name:"Leo Pereira",number:15},
+norway:{name:"Patrick Berg",number:6}
 },
 
 {
 ticket:8,
-brazil:{
-name:"Endrick",
-number:19,
-photo:"assets/brazil/endrick.jpg"
-},
-norway:{
-name:"Antonio Nusa",
-number:20,
-photo:"assets/norway/nusa.jpg"
-}
+brazil:{name:"Ederson Silva",number:2},
+norway:{name:"Morten Thorsby",number:2}
 },
 
 {
 ticket:9,
-brazil:{
-name:"Gabriel Magalhães",
-number:3,
-photo:"assets/brazil/gabriel.jpg"
-},
-norway:{
-name:"Sondre Langås",
-number:24,
-photo:"assets/norway/langas.jpg"
-}
+brazil:{name:"Lucas Paquetá",number:20},
+norway:{name:"Andreas Schjelderup",number:21}
 },
 
 {
 ticket:10,
-brazil:{
-name:"Bruno Guimarães",
-number:8,
-photo:"assets/brazil/bruno.jpg"
-},
-norway:{
-name:"Oscar Bobb",
-number:23,
-photo:"assets/norway/bobb.jpg"
-}
+brazil:{name:"Bruno Guimarães",number:8},
+norway:{name:"Fredrik Aursnes",number:14}
 },
 
 {
 ticket:11,
-brazil:{
-name:"Marquinhos",
-number:4,
-photo:"assets/brazil/marquinhos.jpg"
-},
-norway:{
-name:"Leo Østigård",
-number:4,
-photo:"assets/norway/ostigard.jpg"
-}
+brazil:{name:"Danilo Santos",number:18},
+norway:{name:"Sander Berge",number:8}
 },
 
 {
 ticket:12,
-brazil:{
-name:"Vinícius Júnior",
-number:7,
-photo:"assets/brazil/vini.jpg"
-},
-norway:{
-name:"Alexander Sørloth",
-number:7,
-photo:"assets/norway/sorloth.jpg"
-}
+brazil:{name:"Oscar Bobb",number:22},
+norway:{name:"Oscar Bobb",number:22}
 },
 
 {
 ticket:13,
-brazil:{
-name:"Casemiro",
-number:5,
-photo:"assets/brazil/casemiro.jpg"
-},
-norway:{
-name:"Martin Ødegaard",
-number:10,
-photo:"assets/norway/odegaard.jpg"
-}
+brazil:{name:"Casemiro",number:5},
+norway:{name:"Martin Ødegaard",number:10}
 },
 
 {
 ticket:14,
-brazil:{
-name:"Douglas Santos",
-number:16,
-photo:"assets/brazil/douglas.jpg"
-},
-norway:{
-name:"Julian Ryerson",
-number:26,
-photo:"assets/norway/ryerson.jpg"
-}
+brazil:{name:"Rayan",number:26},
+norway:{name:"Thelo Aasgaard",number:19}
 },
 
 {
 ticket:15,
-brazil:{
-name:"Léo Pereira",
-number:15,
-photo:"assets/brazil/leopereira.jpg"
-},
-norway:{
-name:"Henrik Falchener",
-number:25,
-photo:"assets/norway/falchener.jpg"
-}
+brazil:{name:"Matheus Cunha",number:9},
+norway:{name:"Antonio Nusa",number:20}
 },
 
 {
 ticket:16,
-brazil:{
-name:"Matheus Cunha",
-number:9,
-photo:"assets/brazil/cunha.jpg"
-},
-norway:{
-name:"Kristian Thorstvedt",
-number:18,
-photo:"assets/norway/thorstvedt.jpg"
-}
+brazil:{name:"Luiz Henrique",number:21},
+norway:{name:"Jens Petter Hauge",number:23}
 },
 
 {
 ticket:17,
-brazil:{
-name:"Ederson",
-number:2,
-photo:"assets/brazil/ederson.jpg"
-},
-norway:{
-name:"Sander Berge",
-number:8,
-photo:"assets/norway/berge.jpg"
-}
+brazil:{name:"Endrick",number:19},
+norway:{name:"Torbjørn Heggem",number:17}
 },
 
 {
 ticket:18,
-brazil:{
-name:"Danilo Santos",
-number:18,
-photo:"assets/brazil/danilosantos.jpg"
-},
-norway:{
-name:"Jens Petter Hauge",
-number:13,
-photo:"assets/norway/hauge.jpg"
-}
+brazil:{name:"Gabriel Martinelli",number:22},
+norway:{name:"Julian Ryerson",number:26}
 },
 
 {
 ticket:19,
-brazil:{
-name:"Rayan",
-number:26,
-photo:"assets/brazil/rayan.jpg"
-},
-norway:{
-name:"Andreas Schjelderup",
-number:21,
-photo:"assets/norway/schjelderup.jpg"
-}
+brazil:{name:"Gabriel Magalhães",number:3},
+norway:{name:"Alexander Sørloth",number:7}
 },
 
 {
 ticket:20,
-brazil:{
-name:"Gabriel Martinelli",
-number:22,
-photo:"assets/brazil/martinelli.jpg"
-},
-norway:{
-name:"Thelo Aasgaard",
-number:19,
-photo:"assets/norway/aasgaard.jpg"
-}
+brazil:{name:"Bremer",number:14},
+norway:{name:"Jørgen Strand Larsen",number:11}
 },
 
 {
 ticket:21,
-brazil:{
-name:"Luiz Henrique",
-number:21,
-photo:"assets/brazil/luizhenrique.jpg"
-},
-norway:{
-name:"Torbjørn Heggem",
-number:17,
-photo:"assets/norway/heggem.jpg"
-}
+brazil:{name:"Raphinha",number:11},
+norway:{name:"Erling Haaland",number:9}
 },
 
 {
 ticket:22,
-brazil:{
-name:"Raphinha",
-number:11,
-photo:"assets/brazil/raphinha.jpg"
-},
-norway:{
-name:"Fredrik Bjørkan",
-number:15,
-photo:"assets/norway/bjorkan.jpg"
-}
+brazil:{name:"Vinícius Júnior",number:7},
+norway:{name:"Oscar Bobb",number:22}
 },
 
 {
 ticket:23,
-brazil:{
-name:"Neymar Jr",
-number:10,
-photo:"assets/brazil/neymar.jpg"
-},
-norway:{
-name:"Erling Haaland",
-number:9,
-photo:"assets/norway/haaland.jpg"
+brazil:{name:"Neymar Jr.",number:10},
+norway:{name:"Martin Ødegaard",number:10}
 }
-}
+
 ];
